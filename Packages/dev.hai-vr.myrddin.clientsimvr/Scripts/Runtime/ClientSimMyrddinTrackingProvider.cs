874f9165196147c7f2e78a79ce06e86eadc3c4d5
Haï~ <hai-vr@users.noreply.github.com>
2024-08-08 17:02:04 +0200

    Inject InputBase: - In Core, MyrddinUpdateCoordinator, make inputBase injectable instead of getting it at runtime. - In ClientSimVR, get the injected IClientSimInput and inject it to our own MyrddinUpdateCoordinator.   - This has a weird requirement that IClientSimInput extends ClientSimInputBase, to access the Send functions. Unsure how to make this proper for now.

--- Packages/dev.hai-vr.myrddin.clientsimvr/Scripts/Runtime/ClientSimMyrddinTrackingProvider.cs
+++ Packages/dev.hai-vr.myrddin.clientsimvr/Scripts/Runtime/ClientSimMyrddinTrackingProvider.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Hai.Myrddin.Core.Runtime;
+using UnityEngine;
 using VRC.SDK3.ClientSim;
 using VRC.Udon.Common;
 
@@ -9,6 +10,22 @@ namespace Hai.Myrddin.ClientSimVR.Runtime
     /// Use the prefab in /Internal/ClientSimMyrddinTrackingData.prefab
     public class ClientSimMyrddinTrackingProvider : ClientSimTrackingProviderBase
     {
+        [SerializeField] private MyrddinUpdateCoordinator updateCoordinator;
+        
+        public override void Initialize(IClientSimEventDispatcher eventDispatcher, IClientSimInput input, ClientSimSettings settings, IClientSimPlayerHeightManager heightManager)
+        {
+            base.Initialize(eventDispatcher, input, settings, heightManager);
+
+            if (input is ClientSimInputBase inputBase)
+            {
+                updateCoordinator.ProvideInputBase(inputBase);
+            }
+            else
+            {
+                Debug.LogError("(MyrddinUpdateCoordinator) IClientSimInput instance does not derive from ClientSimInputBase. We do not support this");
+            }
+        }
+
         public override Transform GetHandRaycastTransform(HandType handType) => handType == HandType.RIGHT ? rightHand : leftHand;
 
         public override bool IsVR() => true;
