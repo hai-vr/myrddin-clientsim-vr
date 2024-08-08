9a836feb77f403e13bc79c9385388fa1c4977632
Haï~ <hai-vr@users.noreply.github.com>
2024-08-08 14:02:44 +0200

    Add ClientSim MyrddinTrackingProvider patcher: - MyrddinTrackingProvider is patched into the ClientSimPlayer when ClientSimVR is enabled.

---
+++ Packages/dev.hai-vr.myrddin.clientsimvr/Scripts/Editor/MyrddinClientSimPatcher.cs
@@ -0,0 +1,67 @@
+﻿using System.IO;
+using System.Reflection;
+using Hai.Myrddin.ClientSimVR.Runtime;
+using HarmonyLib;
+using UnityEditor;
+using UnityEngine;
+using VRC.SDK3.ClientSim;
+
+namespace Hai.Myrddin.ClientSimVR.Editor
+{
+    [InitializeOnLoad]
+    public class MyrddinClientSimPatcher
+    {
+        private const string HarmonyIdentifier = "dev.hai-vr.myrddin.clientsimvr.Harmony";
+        private static readonly string PrefabPath = Path.Combine("MyrddinClientSimVR", "ClientSimMyrddinTrackingData");
+        
+        private static readonly Harmony Harm;
+
+        static MyrddinClientSimPatcher()
+        {
+            Harm = new Harmony(HarmonyIdentifier);
+
+            AddClientSimVRInitializer();
+        }
+
+        private static void AddClientSimVRInitializer()
+        {
+            var clientSimPlayerToPatch = typeof(ClientSimPlayer);
+            var theInitializer = clientSimPlayerToPatch.GetMethod("Initialize", BindingFlags.Instance | BindingFlags.Public);
+            var ourPatch = typeof(MyrddinClientSimPatcher).GetMethod(nameof(PatchInitialize));
+            
+            DoPatch(theInitializer, new HarmonyMethod(ourPatch));
+        }
+
+        public static bool PatchInitialize(ClientSimPlayer __instance)
+        {
+            if (PlayerPrefs.GetInt("Hai.Myrddin.ClientSimVR") > 0)
+            {
+                var myrddinTrackingData = Resources.Load<GameObject>(PrefabPath).GetComponent<ClientSimMyrddinTrackingProvider>();
+                
+                var playerTrackingDataField = Field("playerTrackingData");
+                (playerTrackingDataField.GetValue(__instance) as ClientSimTrackingProviderBase).gameObject.SetActive(false);
+                myrddinTrackingData.gameObject.SetActive(true);
+                (Field("reticle").GetValue(__instance) as ClientSimReticle).gameObject.SetActive(false);
+                playerTrackingDataField.SetValue(__instance, myrddinTrackingData);
+                
+                Debug.Log("(MyrddinClientSimPatcher) ClientSimVR is ON: Successfully switched to MyrddinTrackingData");
+            }
+            else
+            {
+                Debug.Log("(MyrddinClientSimPatcher) ClientSimVR is switched OFF");
+            }
+            
+            return true;
+        }
+
+        private static FieldInfo Field(string name)
+        {
+            return typeof(ClientSimPlayer).GetField(name, BindingFlags.Instance | BindingFlags.NonPublic);
+        }
+
+        private static void DoPatch(MethodInfo source, HarmonyMethod destination)
+        {
+            Harm.Patch(source, destination);
+        }
+    }
+}
---
+++ Packages/dev.hai-vr.myrddin.clientsimvr/Scripts/Runtime/ClientSimMyrddinTrackingProvider.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using VRC.SDK3.ClientSim;
+using VRC.Udon.Common;
+
+namespace Hai.Myrddin.ClientSimVR.Runtime
+{
+    /// Myrddin Tracking Provider.<br/>
+    /// A ClientSim tracking provider that integrates with Myrddin.<br/>
+    /// Use the prefab in /Internal/ClientSimMyrddinTrackingData.prefab
+    public class ClientSimMyrddinTrackingProvider : ClientSimTrackingProviderBase
+    {
+        public override Transform GetHandRaycastTransform(HandType handType) => handType == HandType.RIGHT ? rightHand : leftHand;
+
+        public override bool IsVR() => true;
+
+        public override bool SupportsPickupAutoHold() => false;
+
+        public override void LookTowardsPoint(Vector3 point) { }
+    }
+}
